5ffc42f721acd4752ad29a7c1b509ca5f1ee0012
SAROCHA042 <91052520+SAROCHA042@users.noreply.github.com>
2022-08-01 00:51:59 +0700

    Add files via upload

---
+++ src/horoland/HoroLand.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horoland;
+
+/**
+ *
+ * @author DELL
+ */
+public class HoroLand {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here                
+        HoroLand_GUI obj = new HoroLand_GUI();
+        obj.show();
+    }
+}
